0dba2dd381d5afda0519806dd051dc93a0887e91
harshita261003 <118755090+harshita261003@users.noreply.github.com>
2024-06-22 11:02:05 +0530

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CalendarComponent from '../components/CalendarComponent';
+
+const CalendarPage = () => {
+  return (
+    <div>
+      <h1>Calendar</h1>
+      <CalendarComponent />
+    </div>
+  );
+};
+
+export default CalendarPage;
