08a6ee44f538fccab2dfc044be2e31cbbda41987
harshita261003 <118755090+harshita261003@users.noreply.github.com>
2024-06-22 11:00:07 +0530

    Create  chartcomponrnt.js

---
+++ chartcomponrnt.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+
+const data = [
+  { name: 'Page A', uv: 400, pv: 2400, amt: 2400 },
+  { name: 'Page B', uv: 300, pv: 1398, amt: 2210 },
+  // more data
+];
+
+const ChartComponent = () => {
+  return (
+    <LineChart width={600} height={300} data={data}>
+      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+    </LineChart>
+  );
+};
+
+export default ChartComponent;
