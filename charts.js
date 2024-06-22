ca06e5c18f7b2a97a13c12383ee80a5568b45e42
harshita261003 <118755090+harshita261003@users.noreply.github.com>
2024-06-22 11:01:47 +0530

    Create charts.js

---
+++ charts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChartComponent from '../components/ChartComponent';
+
+const Charts = () => {
+  return (
+    <div>
+      <h1>Charts</h1>
+      <ChartComponent />
+    </div>
+  );
+};
+
+export default Charts;
