61d1e4ec6bde11b28bbd2d5611ed36089cd110ea
harshita261003 <118755090+harshita261003@users.noreply.github.com>
2024-06-22 11:01:13 +0530

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChartComponent from '../components/ChartComponent';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <ChartComponent />
+    </div>
+  );
+};
+
+export default Dashboard;
