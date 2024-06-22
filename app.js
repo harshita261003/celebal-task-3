2dfc0649fc82b079e5ab3106b88587188d93a3f6
harshita261003 <118755090+harshita261003@users.noreply.github.com>
2024-06-22 11:02:34 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Sidebar
