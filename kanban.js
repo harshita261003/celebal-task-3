36f44c454a47b854a6b450c6932d4ca77f598944
harshita261003 <118755090+harshita261003@users.noreply.github.com>
2024-06-22 11:02:19 +0530

    Create kanban.js

---
+++ kanban.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import KanbanComponent from '../components/KanbanComponent';
+
+const Kanban = () => {
+  return (
+    <div>
+      <h1>Kanban</h1>
+      <KanbanComponent />
+    </div>
+  );
+};
+
+export default Kanban;
